1f5343d6f042b629f6d776cb8ee97fee0ffe97cc
Laksito Pamilih <laksito@protonmail.com>
2024-07-31 13:00:44 +0700

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('./queries')
+
 const app = express()
 const port = 3456
 
@@ -13,6 +17,12 @@ app.use(
 app.get('/', (req, res) => {
     res.json({ info: 'Node.js, Express, and Postgres API' })
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
